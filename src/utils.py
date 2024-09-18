b14bec53263c9a554981ed126bdc08dc329812e7
Amyliamg <xl2901@nyu.edu>
2024-09-18 08:52:09 -0500

    Step1 Read File

--- src/utils.py
+++ src/utils.py
@@ -1,20 +1,15 @@
-# Step 1 Parsing the program input
+# Part 1 Read the program input
 import yaml
 from os.path import join, dirname, abspath
+
 
 def read_yaml(filename):
     dir = dirname(abspath(__file__))
     yaml_path = join(dir, 'data',filename)
     try:
-        with open(yaml_path, 'r') as file:
+        with open(yaml_path, 'r', encoding='utf-8') as file:
             data = yaml.load(file, Loader=yaml.SafeLoader)
         return data
     except FileNotFoundError:
         print(f"File not found: {yaml_path}")
         return None
-    
- 
-file_name = 'httpSample.yml'  
-data = read_yaml(file_name)
-if data is not None:
-    print(data)
